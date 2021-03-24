6ff0f19cdd67cd403c572e537312015677f4903f
ngupq <ngupq@fsoft.com.vn>
2021-03-24 10:31:58 +0700

    Update 3/24

---
+++ training/java_core/src/com/vmodev/training/concurrency/utilities/lock/PrintingJob.java
@@ -0,0 +1,17 @@
+package com.vmodev.training.concurrency.utilities.lock;
+
+class PrintingJob implements Runnable {
+    private PrinterQueue printerQueue;
+
+    public PrintingJob(PrinterQueue printerQueue)
+    {
+        this.printerQueue = printerQueue;
+    }
+
+    @Override
+    public void run()
+    {
+        System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
+        printerQueue.printJob(new Object());
+    }
+}
